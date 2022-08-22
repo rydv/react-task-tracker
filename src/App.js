bb129ad7ce9191aa8f6c244dbd624a9609d0ada5
Sanjay Yadav <sanjayyadav@Sanjays-MBP.bbrouter>
2022-08-23 00:44:36 +0530

    Task Tracker Day-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from "react"
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks,setTasks] = useState(
     [
     {
@@ -26,15 +28,33 @@ function App() {
     }
 ]
 )
-
+  //Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !==id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id===id ? {...task, reminder: !task.reminder } : task))
+  }
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="container">
-      <Header title='Task Tracker'/>
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}/>) : ('No Tasks To Show')}
+      <Header 
+      title='Task Tracker' 
+      onAdd = {() => setShowAddTask(!showAddTask)}
+      showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} 
+      onDelete={deleteTask}
+      onToggle={toggleReminder}/>) : ('No Tasks To Show')}
     </div>
   );
 }
